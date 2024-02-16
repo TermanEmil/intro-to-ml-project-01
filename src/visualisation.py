0db9f51ac961c2bacd6c98a9a155c75b30dee8c3
TermanEmil <terman.emil@gmail.com>
2024-02-16 17:00:46 +0100

    Visualise in a 2d plot different properties

---
+++ src/visualisation.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot
+
+from main import importData2
+
+
+def visualise():
+    data = importData2().toCentered()
+
+    attributeCombos = [
+        (0, 1), (0, 2), (2, 6),
+        (0, 4), (0, 5), (2, 4),
+    ]
+
+    figure, axs = pyplot.subplots(2, 3)
+    for i, ax in enumerate(axs.flat):
+        attributeIndex1, attributeIndex2 = attributeCombos[i]
+        for classIndex in range(len(data.classNames)):
+            classMask = data.classLabels == classIndex
+            xData = data.X[classMask, attributeIndex1]
+            yData = data.X[classMask, attributeIndex2]
+            ax.plot(xData, yData, 'o', alpha=0.3)
+        ax.set_xlabel(data.attributeNames[attributeIndex1])
+        ax.set_ylabel(data.attributeNames[attributeIndex2])
+    pyplot.show()
+
+
+visualise()
