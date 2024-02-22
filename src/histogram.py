92c8099f4c414ba22ed128835532356376deed22
TermanEmil <terman.emil@gmail.com>
2024-02-22 16:38:40 +0100

    Plot histograms with stacked and superimposed values

--- src/histogram.py
+++ src/histogram.py
@@ -18,22 +18,39 @@ def visualise_histogram():
     y = data.classLabels
     N = data.observationsCount
     attributeName = data.attributeNames
+    binsCount = 10
 
-    figure = plt.figure()
-    figure.suptitle("Seed Histogram")
     u = np.floor(np.sqrt(M))
     v = np.ceil(float(M) / u)
+
+    figure = plt.figure()
+    figure.suptitle("Seed Histogram: Normal")
     for i in range(M):
         subplot(int(u), int(v), i + 1)
         xlabel(attributeName[i])
+        hist(X[:, i], bins=binsCount)
 
-        # Stacked histograms
+    figure = plt.figure()
+    figure.suptitle("Seed Histogram: Stacked")
+    for i in range(M):
+        subplot(int(u), int(v), i + 1)
+        xlabel(attributeName[i])
         classMasks = [data.classLabels == classIndex for classIndex in range(len(data.classNames))]
         histData = [X[mask, i] for mask in classMasks]
-        hist(histData, alpha=0.7, label=data.classNames, stacked=True)
-        # plt.legend(data.classNames, bot)
-
+        hist(histData, alpha=0.7, label=data.classNames, stacked=True, bins=binsCount)
     figure.legend(data.classNames,  bbox_to_anchor=(0.9, 0.2))
+
+    figure = plt.figure()
+    figure.suptitle("Seed Histogram: Superimposed")
+    for i in range(M):
+        subplot(int(u), int(v), i + 1)
+        xlabel(attributeName[i])
+        bins = np.linspace(np.min(X[:, i]), np.max(X[:, i]), binsCount)
+        for classIndex in range(len(data.classNames)):
+            classMask = data.classLabels == classIndex
+            hist(X[classMask, i], alpha=0.5, label=data.classNames, bins=bins)
+    figure.legend(data.classNames, bbox_to_anchor=(0.9, 0.2))
+
     show()
 
 
