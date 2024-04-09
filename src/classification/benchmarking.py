65c32d11bed527292335f117256f9ee12587f973
Emil Terman <terman.emil@gmail.com>
2024-04-09 03:26:47 +0200

    McNemar + setup II
    Setup II was not ran for multipl folds tho

--- src/classification/benchmarking.py
+++ src/classification/benchmarking.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import progressbar
 import torch
-from dtuimldmtools import train_neural_net
+from dtuimldmtools import train_neural_net, mcnemar, correlated_ttest
 from sklearn import model_selection
 from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression
@@ -11,6 +11,13 @@ from sklearn.linear_model import LogisticRegression
 from main import importData2
 
 random_state = 1
+
+# Logistic regression constants
+logistic_regression_max_iterations = 20000
+
+# ANN constants
+ann_max_iterations = 3000
+ann_n_replicates = 1
 
 
 def _build_outer_fold_header_str(fold_i, total_folds, y_train, y_test):
@@ -26,11 +33,10 @@ def _build_outer_fold_header_str(fold_i, total_folds, y_train, y_test):
 def benchmark_logistic_regression_model():
     random.seed(random_state)
     np.random.seed(random_state)
+    torch.random.manual_seed(random_state)
 
     outer_folds_count = 10
     inner_folds_count = 5
-
-    max_iterations = 20000
 
     data = importData2().standardized()
     X = data.X
@@ -69,7 +75,7 @@ def benchmark_logistic_regression_model():
                 model = LogisticRegression(
                     random_state=random_state,
                     multi_class="multinomial", solver='lbfgs',
-                    max_iter=max_iterations,
+                    max_iter=logistic_regression_max_iterations,
                     penalty="l2",
                     C=1 / lambda_values[i]
                 )
@@ -87,7 +93,7 @@ def benchmark_logistic_regression_model():
         optimal_model = LogisticRegression(
             random_state=random_state,
             multi_class="multinomial", solver='lbfgs',
-            max_iter=max_iterations,
+            max_iter=logistic_regression_max_iterations,
             penalty="l2",
             C=1 / optimal_lambda_value
         )
@@ -100,15 +106,23 @@ def benchmark_logistic_regression_model():
     return lambda_value_with_its_test_error_rate
 
 
+def _create_ann_model(M: int, C: int, hidden_neurons_count: int):
+    return lambda: torch.nn.Sequential(
+        torch.nn.Linear(M, hidden_neurons_count),
+        torch.nn.ReLU(),
+
+        torch.nn.Linear(hidden_neurons_count, C),
+        torch.nn.Softmax(dim=1),
+    )
+
+
 def benchmark_ann_model():
     random.seed(random_state)
     np.random.seed(random_state)
+    torch.random.manual_seed(random_state)
 
     outer_folds_count = 10
     inner_folds_count = 5
-
-    max_iterations = 3000
-    n_replicates = 1
 
     # Use the general cross entropy loss for multinomial classification
     loss_fn = torch.nn.CrossEntropyLoss()
@@ -121,16 +135,6 @@ def benchmark_ann_model():
 
     # The complexity parameter is the number of neurons in the only hidden layer
     hidden_units_range = list(range(1, 18 + 1))
-
-    # Define the ANN model
-    def create_ann_model(hidden_neurons_count: int):
-        return lambda: torch.nn.Sequential(
-            torch.nn.Linear(M, hidden_neurons_count),
-            torch.nn.ReLU(),
-
-            torch.nn.Linear(hidden_neurons_count, C),
-            torch.nn.Softmax(dim=1),
-        )
 
     complexity_param_with_its_test_error_rate = np.empty(outer_folds_count, dtype=tuple)
 
@@ -156,12 +160,12 @@ def benchmark_ann_model():
             # Train the model using the range of complexity parameters
             for i in range(len(hidden_units_range)):
                 net, final_loss, learning_curve = train_neural_net(
-                    create_ann_model(hidden_units_range[i]),
+                    _create_ann_model(M, C, hidden_units_range[i]),
                     loss_fn,
                     X=torch.tensor(inner_X_train, dtype=torch.float),
                     y=torch.tensor(inner_y_train, dtype=torch.long),
-                    n_replicates=n_replicates,
-                    max_iter=max_iterations
+                    n_replicates=ann_n_replicates,
+                    max_iter=ann_max_iterations
                 )
                 print("\n\tBest loss: {}\n".format(final_loss))
 
@@ -176,12 +180,12 @@ def benchmark_ann_model():
         optimal_complexity_param = hidden_units_range[optimal_complexity_param_index]
 
         optimal_net, optimal_final_loss, _ = train_neural_net(
-            create_ann_model(optimal_complexity_param),
+            _create_ann_model(M, C, optimal_complexity_param),
             loss_fn,
             X=torch.tensor(outer_X_train, dtype=torch.float),
             y=torch.tensor(outer_y_train, dtype=torch.long),
-            n_replicates=n_replicates,
-            max_iter=max_iterations
+            n_replicates=ann_n_replicates,
+            max_iter=ann_max_iterations
         )
 
         # Determine probability of each class using trained network
@@ -198,6 +202,7 @@ def benchmark_ann_model():
 def benchmark_baseline_model():
     random.seed(random_state)
     np.random.seed(random_state)
+    torch.random.manual_seed(random_state)
 
     outer_folds_count = 10
 
@@ -223,10 +228,121 @@ def benchmark_baseline_model():
     return test_error_rate
 
 
+def benchmark_McNemar():
+    random.seed(random_state)
+    np.random.seed(random_state)
+    torch.random.manual_seed(random_state)
+
+    folds_count = 10
+    runs_count = 1000
+
+    data = importData2().standardized()
+    X = data.X
+    y = data.classLabels
+    N, M = X.shape
+    C = len(data.classNames)
+
+    # Using the lambda from the second fold that gave a test error of 0
+    logistic_regression_optimal_lambda = 0.005992285967408437
+
+    # Using 3 hidden units according to the ANN benchmark results
+    ann_optimal_hidden_units = 3
+
+    y_true = []
+    all_logistic_regression_predictions = []
+    all_ann_predictions = []
+    all_baseline_predictions = []
+
+    try:
+        for run_i in range(runs_count):
+            print('\n', '=' * 20, f'Run {run_i + 1}/{runs_count}', '=' * 20)
+            random_seed = random_state + run_i
+            folder = model_selection.KFold(n_splits=folds_count, shuffle=True, random_state=random_seed)
+            for fold_i, (train_i, test_i) in enumerate(folder.split(X, y)):
+                print('-' * 10, f'fold: {fold_i + 1}/{folds_count}', '-' * 10)
+
+                X_train, y_train = X[train_i, :], y[train_i]
+                X_test, y_test = X[test_i, :], y[test_i]
+
+                y_true.append(y_test)
+
+                # Logistic Regression
+                logistic_regression_model = LogisticRegression(
+                    random_state=random_seed,
+                    multi_class="multinomial", solver='lbfgs',
+                    max_iter=logistic_regression_max_iterations,
+                    penalty="l2",
+                    C=1 / logistic_regression_optimal_lambda
+                )
+                logistic_regression_model.fit(X_train, y_train)
+                logistic_regression_predictions = logistic_regression_model.predict(X_test).T
+                all_logistic_regression_predictions.append(logistic_regression_predictions)
+
+                # ANN
+                ann_net, _, _ = train_neural_net(
+                    _create_ann_model(M, C, ann_optimal_hidden_units),
+                    torch.nn.CrossEntropyLoss(),
+                    X=torch.tensor(X_train, dtype=torch.float),
+                    y=torch.tensor(y_train, dtype=torch.long),
+                    n_replicates=ann_n_replicates,
+                    max_iter=ann_max_iterations
+                )
+                ann_predictions = (torch.max(ann_net(torch.tensor(X_test, dtype=torch.float)), dim=1)[1]).data.numpy()
+                all_ann_predictions.append(ann_predictions)
+
+                # Baseline
+                baseline_model = DummyClassifier(strategy='most_frequent')
+                baseline_model.fit(X_train, y_train)
+                baseline_predictions = baseline_model.predict(X_test)
+                all_baseline_predictions.append(baseline_predictions)
+    except KeyboardInterrupt:
+        min_size = min(
+            len(y_true),
+            len(all_logistic_regression_predictions),
+            len(all_ann_predictions),
+            len(all_baseline_predictions)
+        )
+        y_true = y_true[:min_size]
+        all_logistic_regression_predictions = all_logistic_regression_predictions[:min_size]
+        all_ann_predictions = all_ann_predictions[:min_size]
+        all_baseline_predictions = all_baseline_predictions[:min_size]
+
+    print(f'A total of {len(np.concatenate(y_true))} predictions')
+
+    combinations = [
+        ('Logistic vs ANN', all_logistic_regression_predictions, all_ann_predictions),
+        ('Logistic vs Baseline', all_logistic_regression_predictions, all_baseline_predictions),
+        ('ANN vs Baseline', all_ann_predictions, all_baseline_predictions)
+    ]
+    for title, model_a_predictions, model_b_predictions in combinations:
+        print('\n', '=' * 20, f'{title}')
+        print('McNemar')
+        alpha = 0.05
+        [theta, CI, p] = mcnemar(
+            np.concatenate(y_true),
+            np.concatenate(model_a_predictions),
+            np.concatenate(model_b_predictions),
+            alpha=alpha
+        )
+        print(f'theta = {theta}; CI: {CI}; p-value: {p}')
+
+        print('\nSetup II')
+        loss = 2
+        r = [
+            np.mean(np.abs(model_a_predictions[i] - y_true[i]) ** loss - np.abs(model_b_predictions[i] - y_true[i]))
+            for i in range(len(y_true))
+        ]
+        rho = 1 / folds_count
+        alpha = 0.05
+        p_setupII, CI_setupII = correlated_ttest(r, rho, alpha=alpha)
+        print(f'p_setupII = {p_setupII}; CI_setupII = {CI_setupII}')
+
+
 def main():
-    benchmark_logistic_regression_model()
+    # benchmark_logistic_regression_model()
     # benchmark_ann_model()
     # benchmark_baseline_model()
+    benchmark_McNemar()
     pass
 
 
